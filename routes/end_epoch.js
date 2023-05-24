1555fc020127f53bc8ec053276de357784b02059
Alexis Davidson <alexisdavidson00@gmail.com>
2023-05-24 13:35:48 +0700

    Fix ethers call

--- routes/end_epoch.js
+++ routes/end_epoch.js
@@ -8,17 +8,53 @@ const router = express.Router();
 dotenv.config()
 
 router.post('/', async (req, res) => {
-    console.log("Ending epoch...")
+    console.log("Ending epoch requested...")
 
     const provider = new ethers.providers.JsonRpcProvider(process.env.URL_SEPOLIA_INFURA)
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY_TESTNETS, provider)
+    console.log("wallet address", wallet.address)
 
     const poolMaster = new ethers.Contract(PoolMasterAddress.address, PoolMasterAbi.abi, wallet)
+    console.log("poolMaster address", poolMaster.address)
+    // console.log("poolMaster abi", PoolMasterAbi.abi)
     
-    console.log("Start new epoch...")
+
+    console.log("Getting current phase...")
+    let phase = 0
+    try {
+      phase = parseInt(await poolMaster.getPhase())
+      console.log("phase", phase)
+    
+      if (phase != 2) {
+        res.status(500).json("The epoch is not ended yet");
+        return
+      }
+    } catch (error) {
+      console.log(error)
+      res.status(500).json(error);
+      return
+    }
+
+
+    console.log("Finding out winner...")
+    let winnerId = 0
+    console.log("winnerId", winnerId)
+
+
+    console.log("Ending epoch...")
+    try {
+      await poolMaster.endEpoch(winnerId)
+    } catch (error) {
+      console.log(error)
+      res.status(500).json(error);
+      return
+    }
+    
+
+    console.log("Starting new epoch...")
     const nextRandomNumber = getRandom32Int()
 
-    res.send('Success', poolMaster.address);
+    res.status(200).json({msg: "success " + poolMaster.address});
 });
 
 const getRandom32Int = () => {
@@ -27,7 +63,7 @@ const getRandom32Int = () => {
       temp += Math.round(Math.random());
 
     const randomNum = BigInt(temp);
-    console.log(randomNum.toString());
+    // console.log(randomNum.toString());
     return randomNum.toString()
 }
 
